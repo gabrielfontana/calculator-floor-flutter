99858099ea8c4ac6ff147ffe83d11376a416941f
Gabriel Fontana <gabrielf.junqueira74@gmail.com>
2021-04-20 19:38:24 -0300

    Correção de erro na validação

--- lib/helpers/validator_helper.dart
+++ lib/helpers/validator_helper.dart
@@ -4,11 +4,13 @@ const kZeroValue = 'Não utilize valores iguais a 0';
 
 class ValidatorHelper {
   static String checkValidation(String text) {
+    var value = text;
+    double convertedText = double.tryParse(value.replaceAll(',', '.'));
     if (text.isEmpty)
       return kRequiredField;
     else if (text.startsWith('-'))
       return kNegativeValue;
-    else if (text == '0')
+    else if (convertedText * 1 == 0)
       return kZeroValue;
     else
       return null;
